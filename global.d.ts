08923e67042a36ff22b22ab469d8d61f623c7232
ibrahimSumbul <96423728+ibrahimSumbul@users.noreply.github.com>
2024-12-21 05:39:57 +0300

    commit mesajı

--- global.d.ts
+++
@@ -1,8 +0,0 @@
-import * as THREE from 'three';
-
-declare module '@react-three/fiber' {
-  interface ThreeElements {
-    ambientLight: THREE.AmbientLight;
-    pointLight: THREE.PointLight;
-  }
-}
